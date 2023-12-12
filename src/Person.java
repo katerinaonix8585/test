88b50ee3c9131d8f4c4ba56bc8a3a2f42f73650a
katerinaonix8585 <katerinaonix8585@gmail.com>
2023-12-12 18:45:35 +0100

    rename Employee to Person

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,7 @@ public class Person {
     public String firstName;
     public String lastName;
 
+    public int count;
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
